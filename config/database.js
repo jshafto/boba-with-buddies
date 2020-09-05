7ff23a4e4d6e3f125dd6596fbe65a030acfa2227
jshafto <jpshafto@gmail.com>
2020-09-05 15:02:20 -0400

    clean up config changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const config = require("./index");
 
 const db = config.db;
@@ -17,36 +16,3 @@ module.exports = {
     seederStorage: "sequelize"
   },
 };
-=======
-//? Environment variables may not be needed? Heroku might do that for us. Will need clarification.
-//! will route db files, such as projects do, for familiarity, but no env vars for now.
-//? will need username, password, and database to be similar on all local machines? 
-//! username: TBD
-//! password: TBD or stay null? for simplicity
-//! database: TBD
-
-module.exports = {
-  "development": {
-    "username": "test",
-    "password": null,
-    "database": "test_db",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "seederStorage": "sequelize"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
->>>>>>> master
