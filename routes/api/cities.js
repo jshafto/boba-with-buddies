57612fd217b21e91c542749cc66d14f285597de7
kmcgee92 <kaseycmcgee@gmail.com>
2020-09-08 14:14:13 -0600

    added cities routes

--- routes/api/cities.js
+++ routes/api/cities.js
@@ -10,10 +10,22 @@ const router = express.Router();
 //
 router.get("/", asyncHandler( async (req, res) => {
     const cities = await City.findAll()
-    res.JSON({ cities })
+    res.json({ cities })
 }))
 
 // GET /:id(\\d+)
 // returns a given city's details and associated events
+
+router.get("/:id(\\d+)", asyncHandler( async (req, res) => {
+    
+    const city = await City.findByPk(req.params.id)
+    
+    if(!city){
+        const error = new Error("Couldn't find that city!")
+        error.status = 404
+        res.json({ error })
+    }
+    res.json({ city })
+}))
 
 module.exports = router;
