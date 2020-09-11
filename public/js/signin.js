246531931e630d7302a332091790066d6bd71f50
kmcgee92 <kaseycmcgee@gmail.com>
2020-09-11 11:25:25 -0600

    demouser

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,6 +1,6 @@
 // import { disableFormButton } from "./utils/auth.js";
 
-const form = document.querySelector('#login-form');
+const form = document.querySelector('#login__form--signin');
 
 // disableFormButton('#signin-form input', '#signin-form button');
 
@@ -22,6 +22,42 @@ form.addEventListener('submit', async (e) => {
     });
     const data = await res.json();
     if (!res.ok) {
+        const { message } = data;
+        const errorsContainer = document.querySelector('#errors-container');
+        errorsContainer.innerHTML = message;
+        return;
+    }
+    // redirect to dashboard
+    window.location.href = '/dashboard';
+
+
+
+});
+
+//! NOTE --- this is demo form submission
+const formDemo = document.querySelector('#login__form--demo');
+
+// disableFormButton('#signin-form input', '#signin-form button');
+
+formDemo.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(formDemo);
+    const emailAddress = 'boba@demo.com';
+    const password = 'password';
+    const _csrf = formData.get('_csrf');
+
+    const body = { emailAddress, password, _csrf };
+
+    const res = await fetch('/api/users/token', {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const data = await res.json();
+    if (!res.ok) {
+        console.log('whats wrong')
         const { message } = data;
         const errorsContainer = document.querySelector('#errors-container');
         errorsContainer.innerHTML = message;
