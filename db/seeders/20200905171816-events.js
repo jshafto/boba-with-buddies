7ae7415c800177f5fd679819c5854a1661c9581e
kmcgee92 <kaseycmcgee@gmail.com>
2020-09-11 12:32:17 -0600

    added seed data

--- db/seeders/20200905171816-events.js
+++ db/seeders/20200905171816-events.js
@@ -2,32 +2,161 @@
 
 const db = require('../models');
 const { User, City } = db;
+const randAddress = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const someUser = await User.findOne({
-      where: { emailAddress: "demo@demo.demo" }
+
+    const someUser1 = await User.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
     });
-    const someCity = await City.findOne({
-      where: { name: "Portland" }
+    const someUser2 = await User.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
     });
+    const someUser3 = await User.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someUser4 = await User.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someUser5 = await User.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someUser6 = await User.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someUser7 = await User.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someUser8 = await User.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    
+    
+
+    const someCity1 = await City.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someCity2 = await City.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someCity3 = await City.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someCity4 = await City.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someCity5 = await City.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someCity6 = await City.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someCity7 = await City.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+    const someCity8 = await City.findOne({
+      order: [[Sequelize.fn('RANDOM')]]
+    });
+
+    
     await queryInterface.bulkInsert('Events', [
       {
         date: new Date(2021, 11, 24, 10, 33, 30, 0),
-        address: '42 Wallaby Way',
-        hostId: someUser.id,
-        cityId: someCity.id,
+        address: randAddress.address.streetAddress(),
+        hostId: someUser8.id,
+        cityId: someCity8.id,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         date: new Date(2020, 12, 5, 11, 30, 0, 0),
-        address: '42 Wallaby Way',
-        hostId: someUser.id,
-        cityId: someCity.id,
+        address: randAddress.address.streetAddress(),
+        hostId: someUser1.id,
+        cityId: someCity1.id,
         createdAt: new Date(),
         updatedAt: new Date()
-      }
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser2.id,
+        cityId: someCity2.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser3.id,
+        cityId: someCity3.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser4.id,
+        cityId: someCity4.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser5.id,
+        cityId: someCity5.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser8.id,
+        cityId: someCity8.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser2.id,
+        cityId: someCity2.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser6.id,
+        cityId: someCity6.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser7.id,
+        cityId: someCity7.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser3.id,
+        cityId: someCity3.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        date: new Date(2020, 12, 5, 11, 30, 0, 0),
+        address: randAddress.address.streetAddress(),
+        hostId: someUser4.id,
+        cityId: someCity4.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
     ], { fields: ['date', 'address', 'hostId', 'cityId', 'createdAt', 'updatedAt'] });
   },
 
--- db/seeders/20200905171837-rsvps.js
+++ db/seeders/20200905171837-rsvps.js
@@ -5,12 +5,50 @@ const { User, Event } = db;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const someUser = await User.findOne({
-      where: { emailAddress: "otherdemo@demo.demo" }
-    });
-    const someEvent = await Event.findOne();
+    const someUser1 = await User.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someUser2 = await User.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someUser3 = await User.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someUser4 = await User.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someUser5 = await User.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someUser6 = await User.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    
+    const someEvent1 = await Event.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someEvent2 = await Event.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someEvent3 = await Event.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someEvent4 = await Event.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someEvent5 = await Event.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someEvent6 = await Event.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someEvent7 = await Event.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someEvent8 = await Event.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    const someEvent9 = await Event.findOne({order: [[Sequelize.fn('RANDOM')]]});
+    
+    
+    
     await queryInterface.bulkInsert('Rsvps', [
-      { userId: someUser.id, eventId: someEvent.id, createdAt: new Date(), updatedAt: new Date() }
+      { userId: someUser1.id, eventId: someEvent1.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser2.id, eventId: someEvent1.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser3.id, eventId: someEvent1.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser1.id, eventId: someEvent2.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser2.id, eventId: someEvent2.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser1.id, eventId: someEvent3.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser2.id, eventId: someEvent3.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser3.id, eventId: someEvent3.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser4.id, eventId: someEvent3.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser1.id, eventId: someEvent4.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser2.id, eventId: someEvent4.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser3.id, eventId: someEvent4.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser4.id, eventId: someEvent4.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser5.id, eventId: someEvent4.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser6.id, eventId: someEvent4.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser1.id, eventId: someEvent5.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser2.id, eventId: someEvent5.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser1.id, eventId: someEvent6.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser2.id, eventId: someEvent6.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser3.id, eventId: someEvent6.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser1.id, eventId: someEvent7.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser2.id, eventId: someEvent7.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser1.id, eventId: someEvent8.id, createdAt: new Date(), updatedAt: new Date() },
+      { userId: someUser1.id, eventId: someEvent9.id, createdAt: new Date(), updatedAt: new Date() },
     ], { fields: ['userId', 'eventId', 'createdAt', 'updatedAt'] });
   },
 
