c6c79e45c78f88d8d8f07bb96e75d8d0f4498fea
jshafto <jpshafto@gmail.com>
2020-09-11 17:45:30 -0400

    cleanup git file

--- routes/pages.js
+++ routes/pages.js
@@ -50,17 +50,10 @@ router.get('/boba-times/:id(\\d+)', csrfProtection, (req, res) => {
 // '/dashboard' route
 // redirects to the signin page if user isn't signed in
 router.get('/dashboard', csrfProtection, (req, res) => {
-<<<<<<< HEAD
-    // if (!req.user) {
-    //     res.redirect("/signin");
-    //     return;
-    // }
-=======
     if (!req.user) {
         res.redirect("/login");
         return;
     }
->>>>>>> master
     res.render('dashboard', { nickname: req.user.nickname, csrf: req.csrfToken() });
 });
 
