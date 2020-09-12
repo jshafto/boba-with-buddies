04ea64e648b3a071406f3f75c055fbda4d9f3f72
kmcgee92 <kaseycmcgee@gmail.com>
2020-09-12 15:29:01 -0600

    nicknames test

--- db/seeders/20200905171745-users.js
+++ db/seeders/20200905171745-users.js
@@ -2,23 +2,24 @@
 
 const bcrypt = require('bcryptjs');
 const randName = require('faker')
+const randNickname = require('nicknames')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [
-      { nickname: randName.name.firstName(), emailAddress: 'demo@demo.demo', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      { nickname: randName.name.firstName(), emailAddress: 'otherdemo@demo.demo', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      { nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'demo@demo.demo', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      { nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'otherdemo@demo.demo', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
       {  nickname: 'Boba Buddy', emailAddress: 'boba@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba2@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba3@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba4@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba5@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba6@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba7@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba8@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba9@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba10@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
-      {  nickname: randName.name.firstName(), emailAddress: 'boba11@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba2@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba3@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba4@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba5@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba6@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba7@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba8@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba9@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba10@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
+      {  nickname: randNickname.all[(Math.floor(Math.random()*100)+2)], emailAddress: 'boba11@demo.com', hashedPassword: await bcrypt.hash('password', 10), createdAt: new Date(), updatedAt: new Date() },
     ], { fields: ['nickname', 'emailAddress', 'hashedPassword', 'createdAt', 'updatedAt'] });
   },
 
